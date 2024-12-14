93802d45812eed7fc1b9480fca560abcabb84b08
Israel Antezana Rojas <israelantezana@gmail.com>
2024-12-14 12:44:12 -0400

    palabra de un caracter es palindromo

--- src/palindromo.spec.js
+++ src/palindromo.spec.js
@@ -4,8 +4,13 @@ describe("Palindromo", () => {
   it("cadena vacia no es palindromo", () => {
     expect(palindromo("")).toEqual(false);
   });
+
+  it("palabra de un caracter es palindromo", () => {
+    expect(palindromo("a")).toEqual(true);
+  });
 });
 
 function palindromo(palabra) {
-  return false;
+  if (palabra === "") return false;
+  return true;
 }
