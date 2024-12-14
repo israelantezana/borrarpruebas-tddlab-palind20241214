e116c97d63ef07827001ada9f3fa0648479b2ced
Israel Antezana Rojas <israelantezana@gmail.com>
2024-12-14 13:18:44 -0400

    palabra de dos letras iguales

--- src/palindromo.spec.js
+++ src/palindromo.spec.js
@@ -8,6 +8,10 @@ describe("Palindromo", () => {
   it("palabra de un caracter es palindromo", () => {
     expect(palindromo("a")).toEqual(true);
   });
+
+  it("palabra de un dos letras iguale es palindromo", () => {
+    expect(palindromo("aa")).toEqual(true);
+  });
 });
 
 function palindromo(palabra) {
